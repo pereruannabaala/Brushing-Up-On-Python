7121195cf2d515a60c42356fd7464771fb82e770
pereruannabaala <pereruannabaala@gmail.com>
2025-07-14 13:13:46 +0300

    add uses of comments andadd a comment after a line

--- comments.py
+++ comments.py
@@ -1,2 +1,9 @@
+#Comments can be used to explain Python code.
+#Comments can be used to make the code more readable.
+#Comments can be used to prevent execution when testing code.
+
 #This is a comment
 print ("Hello World")
+
+#Comments can be placed at the end of a line
+print ("Hello world") #This is a comment
