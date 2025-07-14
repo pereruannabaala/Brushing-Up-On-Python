dc15f2e4d84e71db0072b77b69faa4c7784158ab
pereruannabaala <pereruannabaala@gmail.com>
2025-07-14 13:15:17 +0300

    add other uses of comments

--- comments.py
+++ comments.py
@@ -7,3 +7,7 @@ print ("Hello World")
 
 #Comments can be placed at the end of a line
 print ("Hello world") #This is a comment
+
+#A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+#print("Hello world")
+print("Cheers, Mate!")
