1650b91d182ac7c4da41e1a10204fd8220198482
pereruannabaala <pereruannabaala@gmail.com>
2025-07-14 14:20:22 +0300

    unpacking a collection

--- assigning_multiple_values.py
+++ assigning_multiple_values.py
@@ -1,1 +1,19 @@
 # Many values to multiple variables
+x , y, z = "Orange","Banana", "Cherry"
+print (x)
+print (y)
+print (z)
+
+#One value to multiple variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a collection
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+fruits = ["apple", "banana", "cherry"]
+x, y, z =fruits
+print(x)
+print(y)
+print(z)
