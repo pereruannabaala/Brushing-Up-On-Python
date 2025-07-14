bc3c31ef50432539b306b88eef84f5dd60a14b6c
pereruannabaala <pereruannabaala@gmail.com>
2025-07-14 13:28:54 +0300

    show how differnt variable types can be changed

---
+++ variables.py
@@ -0,0 +1,10 @@
+#Variables are containers for storing data values.
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set
+x = 4 #x is of type int
+x = "Sally" #x is of type str
+print(x)
