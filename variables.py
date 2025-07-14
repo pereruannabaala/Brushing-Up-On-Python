63c0e0955412d90ba2657542d0ce5bf5939f48e4
pereruannabaala <pereruannabaala@gmail.com>
2025-07-14 13:34:19 +0300

    show how a data type of a variable can be gotten using the type()function

--- variables.py
+++ variables.py
@@ -15,3 +15,10 @@ x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
 print(x,y,z)
+
+#Get the Type
+#You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
