e3950f0d945d57f65685912b584343660967ca95
pereruannabaala <pereruannabaala@gmail.com>
2025-07-14 13:31:05 +0300

    show how a data type of a variable can be shown using casting

--- variables.py
+++ variables.py
@@ -8,3 +8,10 @@ print(y)
 x = 4 #x is of type int
 x = "Sally" #x is of type str
 print(x)
+
+#Casting
+#If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x,y,z)
